d2c12ddcbb0a0b4d2a6cb4b2b6d174c4bad1015b
Ben West <bb-west@hotmail.co.uk>
2017-06-24 05:35:29 +0100

    This should work

--- javascript.js
+++ javascript.js
@@ -1,3 +1,10 @@
 /**
  * Created by Ben on 24/06/2017.
  */
+
+$( document ).ready(function() {
+
+    $(document).ready(function(){
+        $("#container").hide(0).delay(500).fadeIn(3000)
+    });
+});
