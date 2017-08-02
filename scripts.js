eed3f61c09a3ef1c9bf2e5a0bbf0988a7ae3632c
Ben West <bb-west@hotmail.co.uk>
2017-08-02 02:39:47 +0100

    the rest

--- scripts.js
+++ scripts.js
@@ -1,178 +1,140 @@
-$(document).ready(function () {
-    roundNumber = 1;
-    $nextButton = $('#next');
-    money = 0;
-    roundLoss = 1400*5;
-    roundWin = 3250*5;
-    enemyMoney = $('#enemyMoney');
-    buy = "full";
-    fullBuy = 25000;
-    halfBuy = 10000;
-    consecutiveLosses = 0;
-    savedGuns = 0;
-    kills = 0;
-    savedMoneyPerGun = 4000;
-    $prediction = $('#prediction');
-    $predictionHeader = $('#prediction h2');
-    $predictionParagraph = $('#prediction p');
-    didTheyWin = true;
-    
-    function moneyLimits(){
-        if (money > 80000){
-            money = 80000;
-        } else if (money < 0) {
-            money = 0;
-        }
-    }
-    
-    function calculateLossBonus(){
-        if (consecutiveLosses === 0 || consecutiveLosses === 1){
-            roundLoss = 1400*5;
-        } else if (consecutiveLosses === 2){
-            roundLoss = 1900 * 5;
-        } else if (consecutiveLosses === 3 ){
-            roundLoss = 2400 * 5;
-        } else if (consecutiveLosses === 4){
-            roundLoss = 2900 * 5;
-        } else {
-            roundLoss = 3500 * 5;
-        }
-    }
-    
-
-    $("#roundnumber").html(roundNumber);
-    // UPDATES ROUND AND MONEY
-    function nextRound() {
-        roundNumber++;
-        calculateLossBonus()
-       // moneyLimits();
-        $(enemyMoney).html('$'+money);
-        $("#roundnumber").html(roundNumber);
-        predict();
-        
-    }
-    
-    // SETS MONEY TO NEW VALUE
-    $('#nextWin').on('click', function(){
-        money -= fullBuy;
-        moneyLimits();
-        money += roundWin;
-        consecutiveLosses = 0;
-        money += savedGuns * 4000;
-        money += kills * 300;
-        moneyLimits();
-        didTheyWin = true;
-        nextRound();
-    });
-    $('#nextLose').on('click', function(){
-        consecutiveLosses++;
-        calculateLossBonus();
-        switch (buy) {
-            case "full":
-                money -= fullBuy;
-                moneyLimits();
-                money += roundLoss;
-                break;
-            case "half":
-                money -= halfBuy;
-                moneyLimits();
-                money += roundLoss;
-                break;
-            case "force":
-                money = 0;
-                money += roundLoss;
-                break;
-            case "eco":
-                money += roundLoss;
-                break;
-                  };
-        moneyLimits();
-        money += savedGuns * 4000;
-        money += kills * 300;
-        moneyLimits();
-        didTheyWin = false;
-        nextRound();
-    });
+$(document).ready(function(){
+    var click49 = new Audio('sounds/click49.wav');
+    var click50 = new Audio('sounds/click50.wav');
+    var click51 = new Audio('sounds/click51.wav');
+    var click52 = new Audio('sounds/click52.wav');
+    var click53 = new Audio('sounds/click53.wav');
+    var click54 = new Audio('sounds/click54.wav');
+    var click55 = new Audio('sounds/click55.wav');
+    var click56 = new Audio('sounds/click56.wav');
+    var click57 = new Audio('sounds/click57.wav');
+    var click58 = new Audio('sounds/click58.wav');
+    var click59 = new Audio('sounds/click59.wav');
+    var click60 = new Audio('sounds/click60.wav');
+    var click61 = new Audio('sounds/click61.wav');
+    var click62 = new Audio('sounds/click62.wav');
+    var click63 = new Audio('sounds/click63.wav');
+    var click64 = new Audio('sounds/click64.wav');
+    var click65 = new Audio('sounds/click65.wav');
+    var click66 = new Audio('sounds/click66.wav');
+    var click67 = new Audio('sounds/click67.wav');
+    var click68 = new Audio('sounds/click68.wav');
+    var click69 = new Audio('sounds/click69.wav');
+    var click70 = new Audio('sounds/click70.wav');
+    var click71 = new Audio('sounds/click71.wav');
+    var click72 = new Audio('sounds/click72.wav');
+    var click73 = new Audio('sounds/click73.wav');
+    var click74 = new Audio('sounds/click74.wav');
+    var click75 = new Audio('sounds/click75.wav');
+    var click76 = new Audio('sounds/click76.wav');
+    var click77 = new Audio('sounds/click77.wav');
+    var click78 = new Audio('sounds/click78.wav');
+    var click79 = new Audio('sounds/click79.wav');
+    var click80 = new Audio('sounds/click80.wav');
+    var click81 = new Audio('sounds/click81.wav');
+    var click82 = new Audio('sounds/click82.wav');
+    var click83 = new Audio('sounds/click83.wav');
+    var click84 = new Audio('sounds/click84.wav');
+    var click85 = new Audio('sounds/click85.wav');
+    var click86 = new Audio('sounds/click86.wav');
+    var click87 = new Audio('sounds/click87.wav');
+    var click88 = new Audio('sounds/click88.wav');
+    var click89 = new Audio('sounds/click89.wav');
+    var click90 = new Audio('sounds/click90.wav');
+    var space = new Audio('sounds/space.wav');
     
     
-    //Event Listerners
-    $('#enemybuy input').on('change', function() {
-        buy = $('input[name=whatDidTheyBuy]:checked', '#enemybuy').val();
-    });
+   $('#button').on('click', function(){
+       alert("Hello!");
+   })
     
-    $('#saveGuns input').on('change', function() {
-        savedGuns = parseInt($('input[name=howManySaved]:checked', '#saveGuns').val());
-    });
-    
-     $('#kills input').on('change', function() {
-        kills = parseInt($('input[name=howManyKills]:checked', '#kills').val());
-    });
-    
-    
-    
-    
-    
-    //MAKE THE PREDICTION
-    function predict(){
-        if (roundNumber === 1) {
-            $predictionHeader.html("Prediction: Pistol Round!");
-            $predictionParagraph.html("This is a pistol round, so not much else to say!");
-        } else if (money >= 7000 && money <= 9000) {
-            $predictionHeader.html("Prediction: Force Buy!");
-            $predictionParagraph.html("They won't have enough to full buy next round if they eco, so it makes sense for them to force buy this round.");
-        } else if (money <= 10000 && consecutiveLosses === 2) {
-            $predictionHeader.html("Prediction: Full Eco");
-            $predictionParagraph.html("If they save their money this round, they can full buy next. So expect a full eco. But they could force again, so gain information first. Either way, expect an aggressive play.");
-        } else if (money <= 14000 && consecutiveLosses === 1){
-            $predictionHeader.html("Prediction: Force Buy or Half Buy");
-            $predictionParagraph.html("If they eco this, they still won't have enough for a complete full buy next. But they can do a big force and then only eco once before a full buy. Or alternatively they could half buy. Either way, expect low firepower and low utility.");
-        } else if (money >= 15000 && money <= 20000 && calculateLossBonus <= 2) {
-            $predictionHeader.html("Prediction: Full Eco or Half Buy");
-            $predictionParagraph.html("This depends mostly on their loss bonus. They could force, but if they have rounds to play with, then it would be much wiser to full eco or half buy this round so they can get a full buy next round");
-        } else if (money <= 18000 && consecutiveLosses >= 3) {
-            $predictionHeader.html("Prediction: Half Buy");
-            $predictionParagraph.html("They don't have enough for a full buy, and if they game isn't about to end, then half buying this round and full buying the next round makes the most sense. Expect pistols, armour and a bit of utility.");
-        } else if (money >= 20000 && money <= 24000 && didTheyWin === false) {
-            $predictionHeader.html("Prediction: Full Buy");
-            $predictionParagraph.html("They have enough to pull off a full buy.");
-        } else if (money > 24000 && didTheyWin === false) {
-            $predictionHeader.html("Prediction: Full Buy");
-            $predictionParagraph.html("They're loaded. Expect the fullest of full buys!");
-        } else if (money > 22000 && didTheyWin === true) {
-            $predictionHeader.html("Prediction: Full Buy");
-            $predictionParagraph.html("They won the last round and they have lots of money. Expect a full buy!");
-        } else if (money < 20000 && didTheyWin === true) {
-            $predictionHeader.html("Prediction: Full Buy");
-            $predictionParagraph.html("They won the round, but they're not exactly rich. They will buy, but likely be stuck on rifles with no utility, or SMGs");
-        } else if (didTheyWin === true) {
-            $predictionHeader.html("Prediction: Full Buy");
-            $predictionParagraph.html("They won the last round, so they'll be buying everything they can afford.");
-        } else if (money <= 11000){
-            $predictionHeader.html("Prediction: Force Buy!");
-            $predictionParagraph.html("Either a force, or a full eco. But since you cannot gamble on the full eco, plan for them to force and expect an aggressive strat.");
-        } else if (money > 11000 && money < 20000){
-            $predictionHeader.html("Prediction: Eco or Half Buy");
-            $predictionParagraph.html("Depending on thier loss bonus, they should either eco or half buy. If a low loss bonus, they can eco this and buy next. If a high loss bonus, they can spend a small bit of money on pistols, armour and utility, and still buy next. So get the information, and play accordingly!");
-        } else {
-            $predictionHeader.html("Prediction: No Prediction");
-            $predictionParagraph.html("I am sorry but I don't have a prediction for this. If you can think of one, please email me @ email!");
-        }
-    }
-    
-    predict();
-    
-    
-    //$predictionHeader.html("test");
-    //$predictionParagraph.html("test");
-    
-    $('#popup').hide();
-    
-    $('#info').on('click', function (){
-        $('#popup').slideToggle();
-    })
-    
-    $('#popup #close').on('click', function (){
-        $('#popup').slideToggle();
-    })
-    
+    $( "#target" ).keydown(function(e) {
+        if (e.keyCode == 49){
+            click49.play();
+        } else if (e.keyCode == 50) {
+            click50.play();
+        } else if (e.keyCode == 51) {
+            click51.play();
+        } else if (e.keyCode == 52) {
+            click52.play();
+        } else if (e.keyCode == 53) {
+            click53.play();
+        } else if (e.keyCode == 54) {
+            click54.play();
+        } else if (e.keyCode == 55) {
+            click55.play();
+        } else if (e.keyCode == 56) {
+            click56.play();
+        } else if (e.keyCode == 57) {
+            click57.play();
+        } else if (e.keyCode == 58) {
+            click58.play();
+        } else if (e.keyCode == 59) {
+            click59.play();
+        } else if (e.keyCode == 60) {
+            click60.play();
+        } else if (e.keyCode == 61) {
+            click61.play();
+        } else if (e.keyCode == 62) {
+            click62.play();
+        } else if (e.keyCode == 63) {
+            click63.play();
+        } else if (e.keyCode == 64) {
+            click64.play();
+        } else if (e.keyCode == 65) {
+            click65.play();
+        } else if (e.keyCode == 66) {
+            click66.play();
+        } else if (e.keyCode == 67) {
+            click67.play();
+        } else if (e.keyCode == 68) {
+            click68.play();
+        } else if (e.keyCode == 69) {
+            click69.play();
+        } else if (e.keyCode == 70) {
+            click70.play();
+        } else if (e.keyCode == 71) {
+            click71.play();
+        } else if (e.keyCode == 72) {
+            click72.play();
+        } else if (e.keyCode == 73) {
+            click73.play();
+        } else if (e.keyCode == 74) {
+            click74.play();
+        } else if (e.keyCode == 75) {
+            click75.play();
+        } else if (e.keyCode == 76) {
+            click76.play();
+        } else if (e.keyCode == 77) {
+            click77.play();
+        } else if (e.keyCode == 78) {
+            click78.play();
+        } else if (e.keyCode == 79) {
+            click79.play();
+        } else if (e.keyCode == 80) {
+            click80.play();
+        } else if (e.keyCode == 81) {
+            click81.play();
+        } else if (e.keyCode == 82) {
+            click82.play();
+        } else if (e.keyCode == 83) {
+            click83.play();
+        } else if (e.keyCode == 84) {
+            click84.play();
+        } else if (e.keyCode == 85) {
+            click85.play();
+        } else if (e.keyCode == 86) {
+            click86.play();
+        } else if (e.keyCode == 87) {
+            click87.play();
+        } else if (e.keyCode == 88) {
+            click88.play();
+        } else if (e.keyCode == 89) {
+            click89.play();
+        } else if (e.keyCode == 90) {
+            click90.play();
+        } else if (e.keyCode == 32) {
+            space.play();
+        } 
+    });  
 });
