68cc91bb888de1759fceadc54cf4371d7bbb5249
Ben West <bb-west@hotmail.co.uk>
2017-08-02 02:34:39 +0100

    test

---
+++ ClickSounds/scripts.js
@@ -0,0 +1,140 @@
+$(document).ready(function(){
+    var click49 = new Audio('sounds/click49.wav');
+    var click50 = new Audio('sounds/click50.wav');
+    var click51 = new Audio('sounds/click51.wav');
+    var click52 = new Audio('sounds/click52.wav');
+    var click53 = new Audio('sounds/click53.wav');
+    var click54 = new Audio('sounds/click54.wav');
+    var click55 = new Audio('sounds/click55.wav');
+    var click56 = new Audio('sounds/click56.wav');
+    var click57 = new Audio('sounds/click57.wav');
+    var click58 = new Audio('sounds/click58.wav');
+    var click59 = new Audio('sounds/click59.wav');
+    var click60 = new Audio('sounds/click60.wav');
+    var click61 = new Audio('sounds/click61.wav');
+    var click62 = new Audio('sounds/click62.wav');
+    var click63 = new Audio('sounds/click63.wav');
+    var click64 = new Audio('sounds/click64.wav');
+    var click65 = new Audio('sounds/click65.wav');
+    var click66 = new Audio('sounds/click66.wav');
+    var click67 = new Audio('sounds/click67.wav');
+    var click68 = new Audio('sounds/click68.wav');
+    var click69 = new Audio('sounds/click69.wav');
+    var click70 = new Audio('sounds/click70.wav');
+    var click71 = new Audio('sounds/click71.wav');
+    var click72 = new Audio('sounds/click72.wav');
+    var click73 = new Audio('sounds/click73.wav');
+    var click74 = new Audio('sounds/click74.wav');
+    var click75 = new Audio('sounds/click75.wav');
+    var click76 = new Audio('sounds/click76.wav');
+    var click77 = new Audio('sounds/click77.wav');
+    var click78 = new Audio('sounds/click78.wav');
+    var click79 = new Audio('sounds/click79.wav');
+    var click80 = new Audio('sounds/click80.wav');
+    var click81 = new Audio('sounds/click81.wav');
+    var click82 = new Audio('sounds/click82.wav');
+    var click83 = new Audio('sounds/click83.wav');
+    var click84 = new Audio('sounds/click84.wav');
+    var click85 = new Audio('sounds/click85.wav');
+    var click86 = new Audio('sounds/click86.wav');
+    var click87 = new Audio('sounds/click87.wav');
+    var click88 = new Audio('sounds/click88.wav');
+    var click89 = new Audio('sounds/click89.wav');
+    var click90 = new Audio('sounds/click90.wav');
+    var space = new Audio('sounds/space.wav');
+    
+    
+   $('#button').on('click', function(){
+       alert("Hello!");
+   })
+    
+    $( "#target" ).keydown(function(e) {
+        if (e.keyCode == 49){
+            click49.play();
+        } else if (e.keyCode == 50) {
+            click50.play();
+        } else if (e.keyCode == 51) {
+            click51.play();
+        } else if (e.keyCode == 52) {
+            click52.play();
+        } else if (e.keyCode == 53) {
+            click53.play();
+        } else if (e.keyCode == 54) {
+            click54.play();
+        } else if (e.keyCode == 55) {
+            click55.play();
+        } else if (e.keyCode == 56) {
+            click56.play();
+        } else if (e.keyCode == 57) {
+            click57.play();
+        } else if (e.keyCode == 58) {
+            click58.play();
+        } else if (e.keyCode == 59) {
+            click59.play();
+        } else if (e.keyCode == 60) {
+            click60.play();
+        } else if (e.keyCode == 61) {
+            click61.play();
+        } else if (e.keyCode == 62) {
+            click62.play();
+        } else if (e.keyCode == 63) {
+            click63.play();
+        } else if (e.keyCode == 64) {
+            click64.play();
+        } else if (e.keyCode == 65) {
+            click65.play();
+        } else if (e.keyCode == 66) {
+            click66.play();
+        } else if (e.keyCode == 67) {
+            click67.play();
+        } else if (e.keyCode == 68) {
+            click68.play();
+        } else if (e.keyCode == 69) {
+            click69.play();
+        } else if (e.keyCode == 70) {
+            click70.play();
+        } else if (e.keyCode == 71) {
+            click71.play();
+        } else if (e.keyCode == 72) {
+            click72.play();
+        } else if (e.keyCode == 73) {
+            click73.play();
+        } else if (e.keyCode == 74) {
+            click74.play();
+        } else if (e.keyCode == 75) {
+            click75.play();
+        } else if (e.keyCode == 76) {
+            click76.play();
+        } else if (e.keyCode == 77) {
+            click77.play();
+        } else if (e.keyCode == 78) {
+            click78.play();
+        } else if (e.keyCode == 79) {
+            click79.play();
+        } else if (e.keyCode == 80) {
+            click80.play();
+        } else if (e.keyCode == 81) {
+            click81.play();
+        } else if (e.keyCode == 82) {
+            click82.play();
+        } else if (e.keyCode == 83) {
+            click83.play();
+        } else if (e.keyCode == 84) {
+            click84.play();
+        } else if (e.keyCode == 85) {
+            click85.play();
+        } else if (e.keyCode == 86) {
+            click86.play();
+        } else if (e.keyCode == 87) {
+            click87.play();
+        } else if (e.keyCode == 88) {
+            click88.play();
+        } else if (e.keyCode == 89) {
+            click89.play();
+        } else if (e.keyCode == 90) {
+            click90.play();
+        } else if (e.keyCode == 32) {
+            space.play();
+        } 
+    });  
+});
